751a1a75aad545287d970fa3f4a88ed061b0a5ba
Jose Pedro Saraiva <js@simplesurance.de>
2017-06-12 00:10:14 +0200

    update some test urls

--- bin/run.php
+++ bin/run.php
@@ -6,9 +6,8 @@ require __DIR__ . '/../vendor/autoload.php';
 error_reporting(E_ALL);
 
 $urls = array(
-    'http://vimeo.com/24997574',
     'http://circuspt.blogs.sapo.pt/276442.html',
-    'http://www.publico.pt/Tecnologia/nova-legislacao-para-combate-a-pirataria-no-prazo-maximo-de-um-ano_1500632',
+    'https://www.publico.pt/2017/06/11/mundo/noticia/trump-suspende-visita-ao-reino-unido-1775331',
     'http://www.youtube.com/watch?v=EmLHOGT0v4c&feature=related',
     'http://photosynth.net/',
     'http://www.almadeviajante.com/travelnews/005273.php',
@@ -16,20 +15,25 @@ $urls = array(
     'http://cavalinhoselvagem.blogspot.com/2011/07/aniversariantes-de-julho.html',
     'http://mfmodafeminina.blogs.sapo.pt/21640.html',
     'http://enfermagemnopc.blogs.sapo.pt/707.html',
-    'http://alentejomagazine.com/2006/02/632/',
-    'http://www.noticiasdevilareal.com/noticias/index.php?action=getDetalhe&id=762',
+    'http://www.noticiasdevilareal.com/',
     'http://pplware.sapo.pt/windows/software/auslogics-duplicate-file-finder-remova-ficheiros-repetidos/',
     'http://musica.sapo.pt/noticias/kaossilator_-_sintetizador_de_bolso',
     'http://www.setubalnarede.pt/content/index.php?action=articlesDetailFo&rec=9255',
-    'http://www.ionline.pt/conteudo/7749-europeias-ferreira-leite-apela-ao-dever-civico-que-todos-tem-votar',
+    'https://ionline.sapo.pt/artigo/567448/legislativas-francesas-primeiras-projecoes-colocam-partido-de-macron-em-vantagem?seccao=Mundo_i',
     'http://www.sodoliva.com',
-    'http://www.sabado.pt/Actualidade/Especial-Europeias/Blogues.aspx',
+    'http://www.sabado.pt/',
     'https://www.sugarsync.com/',
     'http://www.jogossantacasa.pt/',
-    'http://www.jn.pt/PaginaInicial/Policia/Interior.aspx?content_id=1902690',
     'http://www.cmjornal.xl.pt/detalhe/noticias/exclusivo-cm/angelico-gnr-investiga-crime-de-homicidio',
     'http://www.agencia.ecclesia.pt/cgi-bin/noticia.pl?id=31713',
     'http://www.jn.pt/PaginaInicial/Mundo/Interior.aspx?content_id=1770243',
+
+//    'https://www.auto1-group.com/',
+//    'https://www.auto1.com/',
+//    'https://www.crunchbase.com/organization/auto1-group',
+//    'https://www.bloomberg.com/research/stocks/private/snapshot.asp?privcapid=337644871',
+//    'https://techcrunch.com/2017/05/22/auto1-group-a-young-used-car-marketplace-serving-europe-is-now-worth-2-8-billion/',
+//    'https://www.stepstone.de/cmp/de/AUTO1-Group-GmbH-140454/jobs.html',
 );
 
 $pe = \HTMLPageExcerpt\Main::createFromIni(__DIR__ . '/../config.ini');
